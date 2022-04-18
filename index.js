09b0ae13ffb0906c862243524d30d05a6a56dfbb
Hiba Ali <hiba.mumin22@gmail.com>
2022-04-18 18:49:18 -0500

    added prompts and input for employee, and added inquirer function

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const generateHTML= require("./src/generateHTML");
 // IMPORTS
-const engineer = require("./lib/engineer");
-const intern = require("./lib/intern");
-const manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
 const inquirer = require("inquirer");
 const path = require("path");
 
@@ -12,7 +12,23 @@ const { run } = require("jest");
 
 //PATH
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUTpath = path.join(OUTPUT_DIR, "team.html");
+
+
+//INQUIRER
+function runInquirer() {
+    inquirer
+      .prompt(employeeQuestionsArray)
+      .then(function (answers) {
+        employeesInformation.push(answers);
+  
+        if (answers.addAnother == "Yes, add another.") {
+          runInquirer();
+        } else {
+          //Filter out Managers and convert to objects
+          const managersData = employeesInformation.filter(({ role }) => {
+            return role == "Manager";
+          });
 
 //VALIDATION FUNCTIONS
 function validateInput(input) {
@@ -21,3 +37,67 @@ function validateInput(input) {
     }
     return "Please enter a value.";
   }
+
+
+
+// EMPLOYEE PROMPTS ARRAYS
+const employeesInformation = [];
+const employeeQuestionsArray = [
+  {
+    type: "input",
+    name: "name",
+    message: "Enter the employee's name:",
+    validate: validateInput,
+},
+{
+  type: "input",
+  name: "id",
+  message: "Enter the employee's ID:",
+  validate: validateInput,
+},
+  {
+    type: "list",
+    name: "role",
+    message: "Choose the employee's role:",
+    choices: ["Manager", "Engineer", "Intern"],
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter the employee's email:",
+    validate: validateEmail,
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "Enter manager's office number:",
+    validate: validateInput,
+    when: function (response) {
+      return response.role == "Manager";
+    },
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter this engineer's GitHub username:",
+    validate: validateInput,
+    when: function (response) {
+      return response.role == "Engineer";
+    },
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "Enter this intern's school name:",
+    validate: validateInput,
+    when: function (response) {
+      return response.role == "Intern";
+    },
+  },
+  {
+    type: "list",
+    name: "addAnother",
+    message: "Add another employee?",
+    choices: ["Yes, add another.", "No, render my new page."],
+  },
+];
