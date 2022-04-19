c3b20bbd2269b75a1a85207a6ff48a59a23ef998
Hiba Ali <hiba.mumin22@gmail.com>
2022-04-18 20:51:19 -0500

    created engineer card

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,5 +1,7 @@
 // RENDER html page 
 const generateTeamPage = function (team) {
+   
+   
     // create Manager card
     const generateManagerCard = function (manager) {
         return `
@@ -20,6 +22,24 @@ const generateTeamPage = function (team) {
     }
 
 
+     // create Engineer card
+     const generateEngineerCard = function (engineer) {
+        return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header bg-secondary">
+                <h3 class="text-white">${engineer.name}</h3>
+                <h4 class="text-white">Engineer</h4><i class="fas fa-glasses"></i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+            </div>
+        </div>
+    </div>
+    `
+    }
 
 
 
